5a53fff13eb907d6f07de0f6fcc4cef66f5d298c
h0wter <howter99@gmail.com>
2022-04-13 21:26:55 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,22 +2,23 @@ import throttle from 'lodash.throttle';
 
 const FORM_DATA_KEY = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
-let formData = {};
 
+let formData = {};
 const savedFormData = JSON.parse(localStorage.getItem(FORM_DATA_KEY));
 
-if (savedFormData && savedFormData.email) {
+if (savedFormData) {
   form.email.value = savedFormData.email;
-}
-if (savedFormData && savedFormData.message) {
   form.message.value = savedFormData.message;
 }
 
 form.addEventListener('input', throttle(onFormInput, 500));
 form.addEventListener('submit', onFormSubmit);
 
-function onFormInput(event) {
-  formData[event.target.name] = event.target.value;
+function onFormInput() {
+  formData = {
+    email: form.email.value,
+    message: form.message.value,
+  };
   localStorage.setItem(FORM_DATA_KEY, JSON.stringify(formData));
 }
 
@@ -26,5 +27,30 @@ function onFormSubmit(event) {
   console.log(formData);
   event.currentTarget.reset();
   localStorage.removeItem(FORM_DATA_KEY);
-  formData = {};
 }
+
+// if (savedFormData && savedFormData.email) {
+//   form.email.value = savedFormData.email;
+//   formData['email'] = savedFormData.email;
+// }
+// if (savedFormData && savedFormData.message) {
+//   form.message.value = savedFormData.message;
+//   formData['message'] = savedFormData.message;
+// }
+
+// form.addEventListener('input', throttle(onFormInput, 500));
+// form.addEventListener('submit', onFormSubmit);
+
+// function onFormInput(event) {
+//   formData[event.target.name] = event.target.value;
+//   localStorage.setItem(FORM_DATA_KEY, JSON.stringify(formData));
+//   console.log(formData);
+// }
+
+// function onFormSubmit(event) {
+//   event.preventDefault();
+//   console.log(formData);
+//   event.currentTarget.reset();
+//   localStorage.removeItem(FORM_DATA_KEY);
+//   formData = {};
+//}
