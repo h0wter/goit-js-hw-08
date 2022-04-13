6773b53d14b62f94617bc05b2a07cf9b2ae85b86
h0wter <howter99@gmail.com>
2022-04-13 14:49:36 +0300

    form done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+import throttle from 'lodash.throttle';
 
+const FORM_DATA_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+let formData = {};
+
+const savedFormData = JSON.parse(localStorage.getItem(FORM_DATA_KEY));
+
+if (savedFormData && savedFormData.email) {
+  form.email.value = savedFormData.email;
+}
+if (savedFormData && savedFormData.message) {
+  form.message.value = savedFormData.message;
+}
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(FORM_DATA_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log(formData);
+  event.currentTarget.reset();
+  localStorage.removeItem(FORM_DATA_KEY);
+  formData = {};
+}
