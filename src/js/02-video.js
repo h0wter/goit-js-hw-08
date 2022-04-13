a9966501bf38f0cb788907b9303f6f1f159753ff
h0wter <howter99@gmail.com>
2022-04-13 13:16:40 +0300

    done video player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const VIDEOPLAYER_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.setCurrentTime(localStorage.getItem(VIDEOPLAYER_KEY));
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+function saveCurrentTime() {
+  player.getCurrentTime().then(seconds => {
+    localStorage.setItem(VIDEOPLAYER_KEY, seconds);
+  });
+}
